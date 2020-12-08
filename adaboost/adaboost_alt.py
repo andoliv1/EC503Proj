7bcc252e2ee5dc55bea04047d0b7a663a9dcc208
ybaker661 <50885811+ybaker661@users.noreply.github.com>
2020-12-08 04:49:50 +0400

    Add files via upload

--- adaboost/adaboost_alt.py
+++ adaboost/adaboost_alt.py
@@ -1,6 +1,7 @@
 import numpy as np
 from WeightedTree import WeightedTree
 
+
 class AdaBst_Alt:
 
     def __init__(self, X, Y, tmax):
@@ -13,9 +14,7 @@ class AdaBst_Alt:
         self.st_w = np.zeros(shape=tmax)
         self.err = np.zeros(shape=tmax)
 
-
     def adaclassifier(self):
-
 
         self.smp_w[0] = np.ones(shape=self.n) / self.n
 
@@ -23,17 +22,17 @@ class AdaBst_Alt:
             # print("This is self.X")
             # print(self.X)
             # print(t)
-            #1) initialize distribution based on 1/n if it is first iteration
-            #or on error weighted predictions of last iteration of not first
+            # 1) initialize distribution based on 1/n if it is first iteration
+            # or on error weighted predictions of last iteration of not first
             curr_smp_w = self.smp_w[t]
-            #2) find and train weak learner
+            # 2) find and train weak learner
             weights = np.zeros(2)
             counter = 0
             # print("This is the official array")
             # print(self.smp_w[t])
             for i in self.Y:
                 # print(self.smp_w[t][counter])
-                if(i == 1):
+                if (i == 1):
                     weights[0] += self.smp_w[t][counter]
                     # print(weights)
                 else:
@@ -41,39 +40,38 @@ class AdaBst_Alt:
                     # print(weights)
                 counter += 1
 
-            print("This is weights")
-            print(weights)
+            # print("This is weights")
+            # print(weights)
 
             stump = WeightedTree(None, 1, None, None, 0)
-            stump = WeightedTree.make_tree(stump, self.X, self.Y, 0,0,curr_smp_w)
-        
-            print("This is dec bound")
-            print(stump.boundary)
+            stump = WeightedTree.make_tree(stump, self.X, self.Y, 0, 0, curr_smp_w)
 
+            # print("This is dec bound")
+            # print(stump.boundary)
 
-            #3) find predictions and error
+            # 3) find predictions and error
             Ypred = WeightedTree.evaluate_data(tree=stump, data=self.X)
             # print(Ypred)
             # print(self.Y)
-            print("current weight")
-            print(curr_smp_w)
+            # print("current weight")
+            # print(curr_smp_w)
             errcurr = curr_smp_w[(Ypred != self.Y)]
             errcurr = sum(errcurr)
-            print("This is the error")
-            print(errcurr)
-            alphat = 0.5*np.log((1 - errcurr) / errcurr)
-            #calculating and renormalizing weight distribution for current stump for later visualization
+            # print("This is the error")
+            # print(errcurr)
+            alphat = 0.5 * np.log((1 - errcurr) / errcurr)
+            # calculating and renormalizing weight distribution for current stump for later visualization
             new_smp_w = (
                     curr_smp_w * np.exp(-alphat * self.Y * Ypred)
             )
             new_smp_w /= new_smp_w.sum()
             if t + 1 < self.tmax:
                 self.smp_w[t + 1] = new_smp_w
-            print("weight after")
-            if t + 1 < self.tmax:
-                print(self.smp_w[t+1])
+            # print("weight after")
+            # if t + 1 < self.tmax:
+                # print(self.smp_w[t + 1])
 
-            #store parameters of weak learners to be used in the aggregate decision making
+            # store parameters of weak learners to be used in the aggregate decision making
             self.st[t] = stump
             self.st_w[t] = alphat
             self.err[t] = errcurr
@@ -86,3 +84,5 @@ class AdaBst_Alt:
         # print(pred)
         # print(self.Y)
         return pred
+
+
--- adaboost/visuals_alt.py
+++ adaboost/visuals_alt.py
@@ -2,11 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from WeightedTree import WeightedTree
 
-## function below plots 2d adaboost results
-def plotres(X, Y, weights = None , DR=None, axes = None, stump = None):
 
-    
-    
+## function below plots 2d adaboost results
+def plotres(X, Y, weights=None, DR=None, axes=None, stump=None):
     if axes is None:
         figure, axes = plt.subplots(figsize=(5, 5), dpi=100)
 
@@ -14,12 +12,10 @@ def plotres(X, Y, weights = None , DR=None, axes = None, stump = None):
     Xcls1 = []
     Xcls2 = []
     for i in range(n):
-        if(Y[i] == 1):
+        if (Y[i] == 1):
             Xcls1 += [X[i, :]]
-        elif(Y[i] == -1):
+        elif (Y[i] == -1):
             Xcls2 += [X[i, :]]
-
-
 
     pad = 1
     x1_min, x1_max = X[:, 0].min() - pad, X[:, 0].max() + pad
@@ -36,121 +32,124 @@ def plotres(X, Y, weights = None , DR=None, axes = None, stump = None):
     # axes.scatter(*Xcls2.T, s=sizes[Y == -1], marker='.', c='royalblue')
     axes.scatter(*Xcls1.T, marker='.', color='mediumvioletred')
     axes.scatter(*Xcls2.T, marker='.', c='royalblue')
-    print(DR)
+
     if DR:
         # print("got here")
         x1, x2 = np.meshgrid(np.arange(x1_min, x1_max, 0.05),
                              np.arange(x2_min, x2_max, 0.05))
-
+        print('eyo')
         # print(x1)
         # print(x2)
-        
+
         # print("This is x1 shape")
         # print(x1.shape)
         # print(np.c_[x1.ravel(), x2.ravel()])
         Ydec = DR.predict(np.c_[x1.ravel(), x2.ravel()])
-        init_1 = 0
-        init_2 = 0
-        counter = 0
-        x1 = x1.ravel()
-        x2 = x2.ravel()
-        for i in Ydec:
-            if(i == 1):
-                if(init_1 == 0):
-                    label_1 = np.array([i])
-                    grid_1 = np.array([[x1[counter],x2[counter]]])
-                    init_1 += 1
-                else:
-                    label_1 = np.concatenate((label_1,[i]),axis = 0)
-                    grid_1 = np.concatenate((grid_1,[[x1[counter],x2[counter]]]),axis = 0)
-            else:
-                if(init_2 == 0):
-                    label_2 = np.array([i])
-                    init_2 += 1
-                    grid_2 = np.array([[x1[counter],x2[counter]]])
-                else:
-                    label_2 = np.concatenate((label_2,[i]),axis = 0)
-                    grid_2 = np.concatenate((grid_2,[[x1[counter],x2[counter]]]),axis = 0)
-            counter += 1
-        
-        print(grid_1)
-        print(grid_2)
+        print('eyo')
+        print(Ydec.size)
+        # init_1 = 0
+        # init_2 = 0
+        # counter = 0
+        # x1 = x1.ravel()
+        # x2 = x2.ravel()
+        # for i in Ydec:
+        #     if (i == 1):
+        #         if (init_1 == 0):
+        #             label_1 = np.array([i])
+        #             grid_1 = np.array([[x1[counter], x2[counter]]])
+        #             init_1 += 1
+        #         else:
+        #             label_1 = np.concatenate((label_1, [i]), axis=0)
+        #             grid_1 = np.concatenate((grid_1, [[x1[counter], x2[counter]]]), axis=0)
+        #     else:
+        #         if (init_2 == 0):
+        #             label_2 = np.array([i])
+        #             init_2 += 1
+        #             grid_2 = np.array([[x1[counter], x2[counter]]])
+        #         else:
+        #             label_2 = np.concatenate((label_2, [i]), axis=0)
+        #             grid_2 = np.concatenate((grid_2, [[x1[counter], x2[counter]]]), axis=0)
+        #     counter += 1
+        #
+        # print(grid_1)
+        # print(grid_2)
 
-        plt.scatter(grid_2[:,0],grid_2[:,1],linewidths = 1,edgecolors='g',alpha=0.1)
-        plt.scatter(grid_1[:,0],grid_1[:,1],linewidths = 1,edgecolors='r',alpha=0.1)
-        plt.title("Scatter plot of decision boundaries")
-        plt.xlabel("x1")
-        plt.ylabel("x2")
+        # plt.scatter(grid_2[:, 0], grid_2[:, 1], linewidths=1, edgecolors='g', alpha=0.1)
+        # plt.scatter(grid_1[:, 0], grid_1[:, 1], linewidths=1, edgecolors='r', alpha=0.1)
+        # plt.title("Scatter plot of decision boundaries")
+        # plt.xlabel("x1")
+        # plt.ylabel("x2")
 
+        # print("This is theresult of predict")
+        Ydec = Ydec.reshape(x1.shape)
 
-        # # print("This is theresult of predict")
-        # Ydec = Ydec.reshape(x1.shape)
+        if sum(np.unique(Ydec)) == 0:
+            class_regions = ['royalblue', 'mediumvioletred']
+        elif sum(np.unique(Ydec)) == 1:
+            class_regions = ['royalblue']
+        else:
+            class_regions = ['mediumvioletred']
 
-        # if sum(np.unique(Ydec)) == 0:
-        #     class_regions = ['mediumvioletred', 'royalblue']
-        # elif sum(np.unique(Ydec)) == 1:
-        #     class_regions = ['mediumvioletred']
-        # else:
-        #     class_regions = ['royalblue']
-
-        # axes.contourf(x1, x2, Ydec, colors=class_regions, alpha=0.1)
+        axes.contourf(x1, x2, Ydec, colors=class_regions, alpha=0.1)
     if stump:
 
-        # x1, x2 = np.meshgrid(np.arange(x1_min, x1_max, 1),
-        #                      np.arange(x2_min, x2_max, 1))
-
-        # # print(x1)
-        # # print(x2)
-        
-        # # print("This is x1 shape")
-        # # print(x1.shape)
-        
-        # Ydec = WeightedTree.evaluate_data(stump,np.c_[x1.ravel(), x2.ravel()])
-        # # print("This is theresult of predict")
-        # Ydec = Ydec.reshape(x1.shape)
-        # Ydec = Ydec
+        # print("got here")
+        x1, x2 = np.meshgrid(np.arange(x1_min, x1_max, 0.05),
+                             np.arange(x2_min, x2_max, 0.05))
+        print('eyo')
         # print(x1)
-        # print(Ydec)
+        # print(x2)
 
-        # if sum(np.unique(Ydec)) == 0:
-        #     class_regions = ['mediumvioletred', 'royalblue']
-        # elif sum(np.unique(Ydec)) == 1:
-        #     class_regions = ['mediumvioletred']
-        # else:
-        #     class_regions = ['royalblue']
+        # print("This is x1 shape")
+        # print(x1.shape)
+        # print(np.c_[x1.ravel(), x2.ravel()])
+        Ydec = WeightedTree.evaluate_data(stump, np.c_[x1.ravel(), x2.ravel()])
+        print('eyo')
+        print(Ydec.size)
+        # print("This is theresult of predict")
+        Ydec = Ydec.reshape(x1.shape)
+        # print(x1)
 
-        # axes.contourf(x1,x2, Ydec, colors=class_regions, alpha=0.1)
-        size = 20
 
-        nx = np.linspace(x1_min,x1_max,size)
-        ny = np.linspace(x2_min,x2_max,size)
+        if sum(np.unique(Ydec)) == 0:
+            class_regions = ['royalblue', 'mediumvioletred']
+        elif sum(np.unique(Ydec)) == 1:
+            class_regions = ['royalblue']
+        else:
+            class_regions = ['mediumvioletred']
 
-        init_1 = 0
-        init_2 = 0
-
-        for i in nx:
-            for j in ny:
-                test_point = np.array([[i,j]])
-                point_eval = WeightedTree.evaluate_point(stump,test_point)
-                if(point_eval == 1):
-                    if(init_1 == 0):
-                        grid_1 = np.array([[i,j]])
-                    else:
-                        grid_1 = np.concatenate((grid_1,[[i,j]]), axis = 0)
-                    init_1 += 1
-                else:
-                    if(init_2 == 0):
-                        grid_2 = np.array([[i,j]])
-                    else:
-                        grid_2 = np.concatenate((grid_2,[[i,j]]), axis = 0)
-                    init_2 += 1
-        
-        plt.subplot(1,1,1)
-        plt.scatter(grid_1[:,0],grid_1[:,1],linewidths = 1,edgecolors='g',alpha=0.1)
-        plt.scatter(grid_2[:,0],grid_2[:,1],linewidths = 1,edgecolors='r',alpha=0.1)
-        plt.title("Scatter plot of decision boundaries")
-        plt.xlabel("x1")
-        plt.ylabel("x2")
+        axes.contourf(x1,x2, Ydec, colors=class_regions, alpha=0.1)
+        # size = 20
+        #
+        # nx = np.linspace(x1_min, x1_max, size)
+        # ny = np.linspace(x2_min, x2_max, size)
+        #
+        # init_1 = 0
+        # init_2 = 0
+        #
+        # for i in nx:
+        #     for j in ny:
+        #         test_point = np.array([[i, j]])
+        #         point_eval = WeightedTree.evaluate_point(stump, test_point)
+        #         if (point_eval == 1):
+        #             if (init_1 == 0):
+        #                 grid_1 = np.array([[i, j]])
+        #             else:
+        #                 grid_1 = np.concatenate((grid_1, [[i, j]]), axis=0)
+        #             init_1 += 1
+        #         else:
+        #             if (init_2 == 0):
+        #                 grid_2 = np.array([[i, j]])
+        #             else:
+        #                 grid_2 = np.concatenate((grid_2, [[i, j]]), axis=0)
+        #             init_2 += 1
+        #
+        # plt.subplot(1, 1, 1)
+        # plt.scatter(grid_1[:, 0], grid_1[:, 1], linewidths=1, edgecolors='g', alpha=0.1)
+        # plt.scatter(grid_2[:, 0], grid_2[:, 1], linewidths=1, edgecolors='r', alpha=0.1)
+        # plt.title("Scatter plot of decision boundaries")
+        # plt.xlabel("x1")
+        # plt.ylabel("x2")
 
     # axis.set_xlim(x1_min + 0.5, x1_max - 0.5)
     # axis.set_ylim(x2_min + 0.5, s2_max - 0.5)
