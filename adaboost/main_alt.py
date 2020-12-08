43599330b92e8c056953385d1139315073f29cad
andoliv1 <andoliv@bu.edu>
2020-12-07 21:51:02 -0300

    ok

--- adaboost/main_alt.py
+++ adaboost/main_alt.py
@@ -9,7 +9,7 @@ from sklearn.datasets import make_gaussian_quantiles
 #initialize sample dataset
 mu = 0
 sigma = 1
-n = 20
+n = 100
 
 # X1 = np.random.uniform(-1, 0.5, size=(10, 2))
 # X2 = np.random.uniform(, 1, size=(10, 2))
@@ -23,14 +23,45 @@ n = 20
 # Y[6:11] = -1
 # print(Y)
 X, Y = make_gaussian_quantiles(n_samples=n, n_classes=2, n_features=2)
+# print(X)
+# print(Y)
 Y = Y*2-1
-tmax = 5
+tmax = 100
 
+# X = np.array([[ 0.91941369, -1.40668594],
+#  [ 0.18477678,  0.42003076],
+#  [-0.03040438, -0.10652165],
+#  [ 0.48855982,  0.06565067],
+#  [-0.05002865,  1.44088418],
+#  [ 0.33019429, -0.87078287],
+#  [-0.58190124,  2.41223044],
+#  [-0.65434502,  0.43796255],
+#  [-0.37328651,  0.07182572],
+#  [-1.2837738 ,  0.40693525],
+#  [-0.66555328,  0.55762218],
+#  [ 0.7444908 , -0.5746354 ],
+#  [ 0.62609619,  0.1515806 ],
+#  [-0.40310026, -0.45315985],
+#  [ 0.8295343 , -1.01863621],
+#  [ 0.07604939,  1.32279631],
+#  [ 0.5957569 , -1.43382616],
+#  [ 0.35238149, -1.36161462],
+#  [-0.73368632, -2.3155115 ],
+#  [-0.91909313, -2.07985754]])
+
+# X = np.array([[1,1],[1,0.5],[1,0.75],[1,1.2]])
+# Y = np.array([1, -1, -1, -1, 1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1])
+# Y= np.array([1,1,-1,-1])
 #get results from adaboost classifier
 res = AdaBst_Alt(X, Y, tmax)
 res.adaclassifier()
-print('hello')
-plotres(X, Y, DR=res, axes=None, weights=None)
+# print('hello')
+Ypred = res.predict(X)
+# print(Ypred)
+# print("hello")
+# print(Y)
+# print(np.sum(Ypred == Y)/(Y.size))
+plotres(X, Y, DR=res, axes=None, weights=None,stump = None)
 #showing iterative results of stump vs ensemble decision real
 # iterlist = list([0, 2, 4])
 #
